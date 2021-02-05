aa03c97a64e2567342ae229ecdc9392992b2c5e5
Steven Dolly <stevendolly12@gmail.com>
2021-02-05 14:56:06 -0600

    Initial upload: read Varian TLog files

---
+++ Library/Utilities/VarianTrajectoryLog.hpp
@@ -0,0 +1,126 @@
+/******************************************************************************/
+/*                                                                            */
+/* Copyright 2020 Steven Dolly                                                */
+/*                                                                            */
+/* Licensed under the Apache License, Version 2.0 (the "License");            */
+/* you may not use this file except in compliance with the License.           */
+/* You may obtain a copy of the License at:                                   */
+/*                                                                            */
+/*     http://www.apache.org/licenses/LICENSE-2.0                             */
+/*                                                                            */
+/* Unless required by applicable law or agreed to in writing, software        */
+/* distributed under the License is distributed on an "AS IS" BASIS,          */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   */
+/* See the License for the specific language governing permissions and        */
+/* limitations under the License.                                             */
+/*                                                                            */
+/******************************************************************************/
+
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// Varian Trajectory Log Manager                                              //
+// (VarianTrajectoryLog.hpp)                                                  //
+//                                                                            //
+// Steven Dolly                                                               //
+// November 30, 2020                                                          //
+//                                                                            //
+// This file contains the header for classes which read Varian trajectory log //
+// files and manage a collection (i.e. database) of these files.              //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef VARIANTLOG_HPP
+#define VARIANTLOG_HPP
+
+#include <string>
+#include <vector>
+
+#include <ctime>
+
+namespace solutio
+{
+  // Dictionary of different axis types (files only provide the integer value)
+  extern std::pair<std::string, int> VarianAxisDictionary[21];
+  // A data struct for a single axis snapshot
+  struct SnapshotSample
+  {
+    std::string Label;
+    float Expected;
+    float Actual;
+  };
+  // The data for the trajectory log file, with read/calculation functions
+  class VarianTrajectoryLog
+  {
+    public:
+      bool IsTrajectoryLog(std::string filename);
+      bool Read(std::string filename);
+      void Print();
+      std::vector<std::string> GetHeader();
+      int GetNumSnapshots(){ return num_snapshots; }
+      std::vector<SnapshotSample> GetSnapshot(int id);
+      // Data Analysis
+      struct AnalysisResults
+      {
+        std::string AxisName = "None";
+        float MaxError = 0.0;
+        float RmsError = 0.0;
+        float MaxRate = 0.0;
+        float AveAbsRate = 0.0;
+      };
+      std::vector<AnalysisResults> Analyze();
+    private:
+      // Data types
+      struct axis_data
+      {
+        uint32_t axis_enum;
+        std::string axis_name;
+        uint32_t num_samples;
+      };
+      struct subbeam_data
+      {
+        uint32_t control_point;
+        float monitor_units;
+        float radiation_time;
+        uint32_t sequence_number;
+        std::string name;
+      };
+      // Header
+      std::string signature;
+      std::string version;
+      uint32_t header_size;
+      uint32_t sampling_interval;
+      uint32_t num_axes_sampled;
+      std::vector<axis_data> axes;
+      uint32_t axis_scale;
+      uint32_t num_subbeams;
+      bool is_truncated;
+      uint32_t num_snapshots;
+      std::string mlc_model;
+      // Subbeam and snapshot data
+      std::vector<subbeam_data> subbeams;
+      std::vector< std::vector<SnapshotSample> > snapshots;
+  };
+  // A database (collection) of trajectory log files
+  class VarianTrajectoryLogDatabase
+  {
+    public:
+      void MakeDatabase(std::string database_path);
+      void Print();
+      int NumLogFiles(){ return tlog_files.size(); }
+      VarianTrajectoryLog GetLogFile(int n);
+      std::string GetLogInfo(int n);
+    private:
+      struct file_data
+      {
+        std::string path = "NA";
+        std::string id = "NA";
+        std::string plan = "NA";
+        std::string field = "NA";
+        std::string date_text = "NA";
+        struct std::tm date;
+      };
+      std::vector<file_data> tlog_files;
+  };
+}
+
+#endif
--- README.md
+++ README.md
@@ -1,42 +1,43 @@
 # SolutioCpp
 
-This C++ library is a collection of theoretical and clinical algorithms for
-applications of physics in medicine, particularly radiation therapy and medical
-imaging. This library can be the foundation for building programs and applications
-used for clinical, research, and education in these medical fields. The repository
-is a companion to [Solutio in silico](solutioinsilico.com), a website designed to
-promote science and technology concepts in medicine.
+This C++ library is a collection of containers and algorithms for applications
+of physics in medicine, particularly radiation therapy (RT) and medical imaging.
+This library can be the foundation for building clinical, research, and
+educational applications in these medical fields. The repository is a companion
+to [Solutio in silico](solutioinsilico.com), a website designed to promote
+science and technology concepts in medicine. More specific documentation for
+this source code can be found on the website.
 
 ## Features
 Key features of SolutioCpp include:
-  * Computer simulation of medical technologies (e.g. X-ray CT)
-  * Algorithms from clinical guideline reports (AAPM TG-43, AAPM TG-71, etc.)
-  * DICOM read/write and database management tools
+  * Basic physics equations, constants, and data relevant to medical physics
+  * Containers to store common imaging/RT components (images, RT dose, etc.)
+  * Read data from DICOM files and other vendor-specific file formats
+  * Simulation of medical technologies (e.g. X-ray CT)
+  * Algorithms from clinical guideline reports (AAPM TG 43, AAPM TG 71, etc.)
 
 ## Install
 [CMake](cmake.org) is used to build the library and examples. The following packages are
 required and need to be installed separately:
   * FFTW
-  * [Grassroots DICOM (GDCM)](http://gdcm.sourceforge.net/wiki/index.php/Main_Page)
+  * DCMTK
+  * ITK
 
-One package (fftw++) is included with the source.
+One package is included with the source (fftw++).
 
 ## Use & Examples
 It is highly recommended to use CMake to compile SolutioCpp with other programs.
-GDCM must be included in any project that uses the DICOM utilities of SolutioCpp.
-See the Examples folder for simple uses of the SolutioCpp library to build other
-programs.
+The above packages will likely also need to be included in projects using SolutioCpp.
+See the Examples folder for simple uses of the SolutioCpp library to build other programs.
 
 ## Project Status
-
 The framework is currently in the pre-release stage (i.e. v0.y.z). The plan is
 for version 1.0.0 to at least include the following features:
-
-  * Table lookup for NIST photon data attenuation coefficient data (NISTPAD)
+  * Read and lookup data for NIST photon attenuation coefficients (NISTX) and electron stopping power ratios (ESTAR)
   * Implementation of the TASMIP algorithm for imaging x-ray source spectra
   * Generation of primary-only CT projection data for various geometric objects (i.e. no Monte Carlo, only ray-tracing and exponential attenuation)
   * Filtered back-projection (FBP) reconstruction algorithm for helical and axial CT projection data
-  * Data container for CT images with DICOM read/write functionality
+  * Reading medical images and RT entities from DICOM files
   * Corrections-based dose calculation for linac photon beams (AAPM TG 71 formalism)
   * Brachytherapy dose calculation (AAPM TG 43 formalism)
   * Linear and logarithmic data interpolation functions for the above features
@@ -51,7 +52,6 @@ In short, the version number is represented as x.y.z, where:
   * z: Patch version (for backwards-compatible bug fixes)
 
 ## License
-
 The framework will be distributed under the Apache 2.0 License, enabling all
 users to develop their own applications for both personal and commercial use.
 The main conditions are that you attach the Apache 2.0 license to any source
